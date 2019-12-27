4104cffb697f96e40b44c4d07dd2333291ed42aa
Nguyễn Duy Toản <nguyenduytoanbkdn@gmail.com>
2019-12-27 17:24:27 +0700

    displaying the list

---
+++ src/app/heroes/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero} from './../../hero.class';
+import { HEROES} from '../../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  hero : Hero = {
+    id:1,
+    name: 'Windstorm'
+  };
+  heroes: Hero[] = HEROES;
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onSelect(value){
+    this.hero = value;
+  }
+
+}
